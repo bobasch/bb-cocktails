3b1d1df0041cb2f4b0b8ce42cfa1c9ab6a424fc6
Júlio Lacerda <julio.castro.lacerda@gmail.com>
2022-10-21 16:18:01 +0200

    Added tests on Store class

---
+++ src/tests/__mocks__/store-ingredients.ts
@@ -0,0 +1,20 @@
+import { AllIngredientsResponse } from "../../api/interface";
+
+export const ingredientsData: AllIngredientsResponse = [
+  {
+    "id": 1,
+    "name": "Vodka"
+  },
+  {
+    "id": 2,
+    "name": "Tonic water"
+  },
+];
+
+export default function ingredients() {
+  return new Promise((resolve, reject) => {
+    process.nextTick(() =>
+      resolve(ingredientsData)
+    );
+  });
+}
---
+++ src/tests/store.test.ts
@@ -0,0 +1,44 @@
+import { Store } from "../store";
+import { ingredientsData } from "./__mocks__/store-ingredients";
+
+describe('testing store file', () => {
+  const store = new Store()
+
+  test("defines fetchIngredients()", () => {
+    expect(typeof store.fetchIngredients).toBe("function");
+  });
+
+  test("ingredients to be a list of {id: number: name: string}", async () => {
+
+    // Prepare a spy for the given method
+    const fetchIngredientsSpy = jest.spyOn(store, "fetchIngredients")
+
+    // mock its returned value
+    fetchIngredientsSpy.mockResolvedValue(ingredientsData)
+
+    // call
+    await store.fetchIngredients();
+
+    // expect call
+    expect(fetchIngredientsSpy).toBeCalledTimes(1);
+
+    // expect it to be an array
+    expect(Array.isArray(store.ingredients)).toBe(true);
+
+    // expect every element to match a given type
+    Array.from(store.ingredients || []).forEach((ingredient) => {
+
+      expect(ingredient).toEqual(
+        expect.objectContaining({
+          "id": expect.any(Number),
+          "name": expect.any(String),
+        })
+      );
+    });
+
+    // restore the mock and clear the state
+    fetchIngredientsSpy.mockClear();
+
+  });
+
+});
